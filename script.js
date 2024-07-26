a553ef006986e5015f113ec95f90cc6b0ba40d3f
palomaferrei <pf1879545@gmail.com>
2024-07-26 08:06:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('caixa-perguntas');
+const caixaAlternativas = document.querySelector('caixa-alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const textoResultado = document.querySelector('texto-resultado');
+ 
+const perguntas = [ 
+    { 
+       enunciado: "Pergunta 1",
+       alternativa: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativa: ["Alternativa 1", "Alternativa 2"],
+    },
+];
+
+function mostraPergunta() {
+    
+}
